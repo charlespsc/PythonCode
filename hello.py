587a7e48c4f1da103119e7a1b5339f844ac0879f
Charles Pereira <charlespereira.fln@gmail.com>
2021-10-13 04:23:09 -0300

    main

--- hello.py
+++ hello.py
@@ -1,1 +1,53 @@
-print ("hello, World!")
+# A função print()
+print ("Hello, Python!")
+
+# A função print() - instruções
+print("The itsy bitsy spider climbed up the waterspout.")
+print("Down came the rain and washed the spider out.")
+
+# A função print() - instruções (função vazia)
+print("The itsy bitsy spider climbed up the waterspout.")
+print()
+print("Down came the rain and washed the spider out.")
+
+# A função print() - os carateres de escape e de newline
+print("The itsy bitsy spider\nclimbed up the waterspout.")
+print()
+print("Down came the rain\nand washed the spider out.")
+print("\\")
+
+#A função print() - utilizar múltiplos argumentos
+print("The itsy bitsy spider" , "climbed up" , "the waterspout.")
+
+#A função print() - a forma posicional de passar os argumentos
+print("My name is", "Python.")
+print("Monty Python.")
+
+# A função print() - os argumentos de keyword (end)
+print("My name is", "Python.", end=" ")
+print("Monty Python.")
+
+print("My name is", "Python.", end="\n")
+print("Monty Python.")
+
+#A função print() - os argumentos de keyword (sep)
+print("My", "name", "is", "Monty", "Python.", sep="->")
+
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+
+print("Programming","Essentials","in", sep="***", end="...")
+print("Python")
+
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+
+print("    *\n" "   * *\n" "  *   *\n" " *     *\n""***   ***\n""  *   *\n""  *   *\n""  *****")
+
+print ("string" * 2)
