f5fa8c7bce3e1848c7e820f8bce19ef6986ca375
Charles Pereira <charlespereira.fln@gmail.com>
2021-11-10 17:59:54 -0300

    Aula101121

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-# A função print()
+print ("A função print()")
 print ("Hello, Python!")
 
 # A função print() - instruções
@@ -56,3 +56,41 @@ print ("string" * 2)
 # 2.2.1.2 Literais - os dados em si
 print("2")
 print(2)
+
+# Inteiros: números octais e hexadecimais
+print(0o123)
+print(0x123)
+
+# Inteiros vs floats
+# 4 é um número inteiro 
+# 4.0 é um floating-point
+# 
+# Floating pode usar NOTAÇÃO CIENTÍFICA
+# Exe.: 3e8
+print ( )
+# Strings (aspas dentro da string)
+print("I like \"Monty Python\"")
+print('I like "Monty Python"')
+print ( )
+print('I\'m Monty Python.')
+print("I'm Monty Python.")
+
+# Valores Booleanos
+print(True > False)
+print(True < False)
+
+print ("I\'m")
+print ('""learning""')
+print ('"""Python"""')
+
+#Python como uma calculadora
+print(2+2)
+
+#Operadores aritméticos: exponenciação
+#Um sinal ** (duplo asterisco) é um operador de exponenciação (potência)
+# exemplo, 2 ** 3
+
+print ("Operadores aritméticos: divisão inteira")
+#Um sinal // (dupla barra) é um operador de divisão inteira.
+print(6 // 4)
+print(6. // 4)
