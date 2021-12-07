11105007df1b8e747c88f1bec7f20409f39c7752
Charles Pereira <charlespereira.fln@gmail.com>
2021-12-07 01:37:53 -0300

    main

--- Module03.py
+++ Module03.py
@@ -1,12 +1,14 @@
 print ("Comparacao: operador de igualdade")
 print ("Qual o resultado?")
 print ("1 == 2 is ->", 1 == 2)
-
-print ("PRACTICE")
 var = 0  # Assigning 0 to var
 print(var == 0)
 var = 1  # Assigning 1 to var
 print(var == 0)
-
+print ()
 print ("Desigualdade: o operador nao igual a (!=)")
+var = 0  # Assigning 0 to var
+print(var != 0)
+var = 1  # Assigning 1 to var
+print(var != 0)
 
