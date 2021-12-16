b4b815c7ef5d2fb58c8d4878371df29a58b790fe
Charles Pereira <charlespereira.fln@gmail.com>
2021-12-16 00:53:55 -0300

    LAB

---
+++ Notes-test.py
@@ -0,0 +1,8 @@
+plant = input("Enter the plant name: ")
+
+if plant = Spathiphyllum:
+    print("Yes - Spathiphyllum is the best plant ever!")
+
+    else:
+    print("No, I want a big Spathiphyllum!")
+    
