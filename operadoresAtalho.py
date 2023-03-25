0073ab7c836a3784f06d53b97d43b5dbbaaf334d
Charles Pereira <charlespereira.fln@gmail.com>
2023-03-25 12:47:17 -0300

    op

---
+++ operadoresAtalho.py
@@ -0,0 +1,23 @@
+print("Operadores atalhos\n")
+#
+# Por exemplo, se precisamos calcular uma série de valores sucessivos de potências de 2
+# Podemos usar uma parte como esta:
+# x = x * 2
+# Você pode usar uma expressão como essa se não conseguir adormecer:
+# sheep = sheep + 1
+#
+# No Python podemos fazer assim:
+# x *= 2
+# sheep += 1
+i = 2
+j = 4
+ 
+i = i + 2 * j
+print("Expressão: ", i)
+#
+ii = 2
+jj = 4
+ii += 2 * jj
+print("Operador de Atalho: ", ii)
+
+ 
