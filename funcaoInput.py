a65df1bad6f6954499bf4909da6f032d810cb34b
Charles Pereira <charlespereira.fln@gmail.com>
2023-03-26 08:53:14 -0300

    input1

--- funcaoInput.py
+++ funcaoInput.py
@@ -1,11 +1,32 @@
 # Funçao input()
 #
-print("Conta-me qualquer coisa...")
-anything = input()
-print("Hum...", anything, "... Realmente?\n")
-
-# A função input() com um argumento
+# print("Conta-me qualquer coisa...")
+# anything = input()
+# print("Hum...", anything, "... Realmente?\n")
+# #
+# #
+# # A função input() com um argumento
+# #
+# anything = input("Conta-me qualquer coisa...\n")
+# print("Hum...", anything, "...Realmente?")
 #
-anything = input("Conta-me qualquer coisa...\n")
-print("Hum...", anything, "...Realmente?")
- 
+#
+# O resultado da função input()
+#
+# anything = input("Digite um número: ")
+# something = anything ** 2.0
+# print(anything, "elevado a 2 é", something)
+#
+#  Digite um número: 2
+# Traceback (most recent call last):
+#   File "/Users/charlespereira/Documents/Repository Github/PythonCode/funcaoInput.py", line 17, in <module>
+#     something = anything ** 2.0
+#                 ~~~~~~~~~^^~~~~
+# TypeError: unsupported operand type(s) for ** or pow(): 'str' and 'float'
+# -----------------------------------------------------------------------------------------------------------
+#
+# Conversão de tipo (tipo de conversões)
+#
+anything = float(input("Digite um número: "))
+something = anything ** 2.0
+print(anything, "elevado a 2 é", something)
