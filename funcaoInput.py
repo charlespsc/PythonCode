895f2608c8ac9c243d2d739545c057e7eee968c4
Charles Pereira <charlespereira.fln@gmail.com>
2023-03-26 08:36:32 -0300

    input

---
+++ funcaoInput.py
@@ -0,0 +1,11 @@
+# Funçao input()
+#
+print("Conta-me qualquer coisa...")
+anything = input()
+print("Hum...", anything, "... Realmente?\n")
+
+# A função input() com um argumento
+#
+anything = input("Conta-me qualquer coisa...\n")
+print("Hum...", anything, "...Realmente?")
+ 
