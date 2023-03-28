9f67e0fba119efe440ddfd4b8993f8431d75e445
Charles Pereira <charlespereira.fln@gmail.com>
2023-03-27 22:33:30 -0300

    lab

---
+++ Lab-SFA-6.py
@@ -0,0 +1,19 @@
+# LAB   Operadores e Expressões – 2
+#
+# Cenário
+# Sua tarefa é preparar um código simples capaz de avaliar a hora de término de um período, 
+# dado como um número de minutos (pode ser arbitrariamente grande).  
+# A hora de início é fornecida como um par de horas (0..23) e minutos (0..59). 
+# O resultado deve ser impresso no console.
+# Por exemplo, se um evento começa às 12:17 e dura 59 minutos, termina às 13:16.
+# Não se preocupe com imperfeições no código - tudo bem se ele aceitar um tempo inválido - 
+# o mais importante é que o código produz resultados válidos para dados de entrada válidos.
+# Teste seu código com cuidado. Dica: usar o operador % pode ser a chave para o sucesso.
+#
+hour = int(input("Hora de início (horas): "))
+mins = int(input("Hora de início (minutos): "))
+dura = int(input("Duração do evento (minutos): "))
+
+# Escreva seu código aqui.
+
+
