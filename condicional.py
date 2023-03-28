682f7a1c816bba1b14c0bff29378e6601a170990
Charles Pereira <charlespereira.fln@gmail.com>
2023-03-28 19:40:23 -0300

    ifs

---
+++ condicional.py
@@ -0,0 +1,50 @@
+# Condições e execução condicional
+#
+print("Condições e execução condicional\n")
+print("Execução condicional: o comando if")
+#
+# if sheep_counter >= 120:      # Avaliar uma expressão de teste
+#     sleep_and_dream()         # Execute se a expressão de teste for verdadeira
+#
+# if sheep_counter >= 120:
+#     make_a_bed()
+#     take_a_shower()
+#     sleep_and_dream()
+# feed_the_sheepdogs()
+#
+print("Execução condicional: o comando if-else")
+# if true_or_false_condition:
+#     perform_if_condition_true
+# else:
+#     perform_if_condition_false
+# 
+# O comando if-else: mais execução condicional
+# if the_weather_is_good:
+#     go_for_a_walk()
+# else:
+#     go_to_a_theater()
+# have_lunch()
+#
+# if the_weather_is_good:
+#     go_for_a_walk()
+#     have_fun()
+# else:
+#     go_to_a_theater()
+#     enjoy_the_movie()
+# have_lunch()
+#
+# Comandos if-else aninhados
+# if the_weather_is_good:
+#     if nice_restaurant_is_found:
+#         have_lunch()
+#     else:
+#         eat_a_sandwich()
+# else:
+#     if tickets_are_available:
+#         go_to_the_theater()
+#     else:
+#         go_shopping()
+#
+# O comando elif
+#
+
