77abc3a80ca1e74f9e11ad7a484e5f7ff59e6f4e
Charles Pereira <charlespereira.fln@gmail.com>
2023-03-28 19:05:15 -0300

    comp

---
+++ comparar.py
@@ -0,0 +1,27 @@
+
+print("Igualdade: o operador de igualdade (==)")
+print(2 == 2)
+print(2 == 2.)
+print(1 == 2)
+#print(var == 0)
+
+var = 0 # Atribuindo 0 a var
+print(var == 0)
+
+var = 1 # Atribuindo 1 a var
+print(var == 0)
+
+print("\nDesigualdade: o operador de desigualdade (!=)")
+# O operador != (Não é igual a) 
+var = 0  # Atribuindo 0 a var
+print(var != 0)
+ 
+var = 1  # Atribuindo 1 a var
+print(var != 0)
+
+print("\nOperadores de comparação: maior ou igual a")
+# notação aritmética clássica: >= (maior que ou igual a).
+print("     centigrade_outside >= 0.0  # Maior ou igual a\n")
+print("Comparação de operadores: menor que e menor ou igual que")
+print("     current_velocity_mph < 85  # Menor que")
+print("     current_velocity_mph <= 85  # Menos que ou igual a\n")
