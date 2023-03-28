4f5db058b6edf4a14b048b92567ccadb1fda8959
Charles Pereira <charlespereira.fln@gmail.com>
2023-03-27 22:25:52 -0300

    lab

--- Lab-SFA-5.py
+++ Lab-SFA-5.py
@@ -1,8 +1,11 @@
 # LAB   Operadores e Expressões
 #
+# Meu cálculo:
 x = float(input("Digite o valor para x: "))
-
-# Escreva seu código aqui.
-y = 1 / (x + (1 / (x + (1 / (x / (1 / x))))))
-
+y = 1 / (x + (1 / (x + (1 / (x + (1 / x))))))
+print("y =", y)
+#
+# Exemplos do Sistema:
+x = float(input("Digite o valor para x: "))
+y = 1./(x + 1./(x + 1./(x + 1./x)))
 print("y =", y)
