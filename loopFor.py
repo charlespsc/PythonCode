b6427960e2c3dbe76a65d7c2e1c4fd408dc38e84
Charles Pereira <charlespereira.fln@gmail.com>
2023-04-19 01:01:32 -0300

    loops

---
+++ loopFor.py
@@ -0,0 +1,11 @@
+# Se você quiser usar o loop while, pode ser assim:
+
+# i = 0
+# while i < 100:
+#     # do_something()
+#     i += 1
+    
+# for i in range(100):
+#     # do_something()
+#     pass
+
