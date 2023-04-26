474cec74b4d519e226a77fe97f1eead11a1cb5ea
Charles Pereira <charlespereira.fln@gmail.com>
2023-04-26 14:33:55 -0300

    for

--- loopFor.py
+++ loopFor.py
@@ -9,3 +9,21 @@
 #     # do_something()
 #     pass
 
+# for i in range(10):
+#    print("O valor de i é atualmente", i)
+
+# # Função range com um argumento.
+# for i in range(2, 8):
+#     print("O valor de i é atualmente", i)
+    
+# Função range com 3 argumento (o terceiro argumento é um incremento).
+for i in range(2, 9, 3):
+  print("O valor de i é atualmente", i)
+
+    # Assim como aqui - não haverá saída:
+
+    # for i in range(1, 1):
+    #     print("O valor de i é atualmente", i)
+    #
+    # for i in range(2, 1):
+    #     print("O valor de i é atualmente", i)
