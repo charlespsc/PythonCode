f59e539a4eef334279ffcb1e988bd37c2e0fcc28
Charles Pereira <charlespereira.fln@gmail.com>
2023-04-26 15:59:42 -0300

    loopfor

--- loopFor.py
+++ loopFor.py
@@ -17,13 +17,20 @@
 #     print("O valor de i é atualmente", i)
     
 # Função range com 3 argumento (o terceiro argumento é um incremento).
-for i in range(2, 9, 3):
-  print("O valor de i é atualmente", i)
+# for i in range(2, 9, 3):
+#   print("O valor de i é atualmente", i)
 
-    # Assim como aqui - não haverá saída:
+#     # Assim como aqui - não haverá saída:
 
+    # Nota: se o conjunto gerado pela função range() estiver vazio, o loop não executará seu corpo.
     # for i in range(1, 1):
     #     print("O valor de i é atualmente", i)
     #
     # for i in range(2, 1):
     #     print("O valor de i é atualmente", i)
+    #
+# A variável expo é usada como uma variável de controle para o loop e indica o valor atual do expoente.
+power = 1
+for expo in range(16):
+  print("2 à potência de", expo, "é", power)
+  power *= 2
