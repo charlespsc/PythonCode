299fd33d0f61f0261b4b0a57d82813f7e9d78c9d
Charles Pereira <charlespereira.fln@gmail.com>
2023-04-28 10:19:58 -0300

    logicaeope

--- logicaEOperacoesBitABit.py
+++ logicaEOperacoesBitABit.py
@@ -12,5 +12,17 @@
 
 # 3.3.3 Valores lógicos vs. bits únicos
  
-i = 1
-j = not not i
+# i = 1
+# j = not i
+
+# 3.3.4 Operadores bit a bit
+
+# Aqui estão todos eles:
+
+# & (e comercial) - conjunção bit a bit;
+# | (barra) - disjunção bit a bit;
+# ~ (til) - negação bit a bit;
+# ^ (circunflexo) ‒ bit a bit exclusivo ou (xor).
+
+
+
