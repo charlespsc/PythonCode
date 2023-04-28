4f827d94524373f576299dae2731993248ec6c3a
Charles Pereira <charlespereira.fln@gmail.com>
2023-04-28 00:27:58 -0300

    logicaEOperadores

---
+++ logicaEOperacoesBitABit.py
@@ -0,0 +1,16 @@
+#
+# 3.3.2 Expressões lógicas
+
+# var = 1
+# # Exemplo 1:
+# print(var > 0)
+# print(not (var <= 0))
+ 
+# # Exemplo 2:
+# print(var != 0)
+# print(not (var == 0))
+
+# 3.3.3 Valores lógicos vs. bits únicos
+ 
+i = 1
+j = not not i
