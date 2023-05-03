0fe9edff45e27ed9fd3e918303234f6bce4608ad
Charles Pereira <charlespereira.fln@gmail.com>
2023-05-02 21:59:02 -0300

    listas

---
+++ Lab-SFA-18.py
@@ -0,0 +1,13 @@
+# 3.4.6   LAB   Básico de listas
+
+# Cenário:
+    
+# Era uma vez um chapéu. O chapéu não continha coelhos, mas uma lista de cinco números: 1, 2, 3, 4 e 5.
+
+# Sua tarefa:
+
+# escreva uma linha de código que solicite que o usuário substitua o número do meio na lista por um número inteiro inserido pelo usuário (Etapa 1)
+# escreva uma linha de código que remova o último elemento da lista (Etapa 2)
+# escreva uma linha de código que imprima o comprimento da lista atual (Etapa 3).
+# Pronto para este desafio?
+
--- listas.py
+++ listas.py
@@ -32,4 +32,31 @@
 
 # print ("\n List length:", len (numbers)) # Imprimindo o comprimento da lista.
 
-3.4.4 Remover elementos de uma lista
+# 3.4.4 Remover elementos de uma lista
+
+# numbers = [10, 5, 7, 2, 1]
+# print("Conteúdo da lista original:", numbers) # Imprimindo conteúdo da lista de originais.
+
+# numbers[0] = 111
+# print("\nConteúdo da lista anterior:", numbers) # Imprimindo conteúdo da lista anterior.
+
+# numbers[1] = numbers[4] # Copiando o valor do quinto elemento para o segundo.
+# print("Conteúdo da lista anterior:", numbers) # Imprimindo conteúdo da lista anterior.
+
+# print ("\nComprimento da lista:", len (numbers)) # Imprimindo comprimento de lista anterior.
+
+# ###
+
+# del numbers[1] # Removendo o segundo elemento da lista.
+# print ("Comprimento da nova lista:", len (numbers)) # Imprimindo novo comprimento da lista.
+# print ("\nNova lista de conteúdo:", numbers) # Imprimindo conteúdo da lista atual.
+
+# ###
+
+
+
+# 3.4.5 Os índices negativos são legais
+
+numbers = [111, 7, 2, 1]
+print(numbers[-4])
+
