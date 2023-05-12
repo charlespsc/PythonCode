9d5e60e7e85f05c935e53cfcbf6ca4a1177acf35
Charles Pereira <charlespereira.fln@gmail.com>
2023-05-12 12:13:57 -0300

    fun

---
+++ funcoes.py
@@ -0,0 +1,58 @@
+
+#################################
+### 4.1.4 Sua primeira função ###
+#################################
+
+# print("Entre um valor: ")
+# a = int(input())
+
+# print("Entre um valor: ")
+# b = int(input())
+
+# print("Entre um valor: ")
+# c = int(input())
+
+# FUNÇÃO -> define = def
+
+# def function_name():
+#     function_body
+
+# Estamos prontos para definir nossa função de prompt. Vamos dar o nome de message - aqui está:
+
+# def message():
+#     print("Entre um valor: ")
+    
+# def message():
+#     print("Entre um valor: ")
+ 
+# print("Começamos aqui.")
+# print("terminamos aqui.")
+
+# Isso significa que o Python lê as definições da função e as lembra, mas não inicia nenhuma sem a sua permissão.
+
+# Nós modificamos o código agora - inserimos a chamada da função entre as mensagens de início e fim:
+# def message():
+#     print("Entre um valor: ")
+ 
+# print("Começamos aqui.")
+# message()
+# print("terminamos aqui.")
+
+
+# Vamos retornar ao nosso exemplo principal e empregar a função para o trabalho certo, como aqui:
+    
+def message():
+    print("Entre um valor: ")
+ 
+message()
+a = int(input())
+message()
+b = int(input())
+message()
+c = int(input())
+
+
+#############################
+### 4.1.6 RESUMO DA SEÇÃO ###
+#############################
+
