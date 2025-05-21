2914bb793eab4d0a97e93ccbf80a41b5c36fdacc
Charles Pereira <81668900+charlespsc@users.noreply.github.com>
2025-05-20 23:30:08 -0300

    Add files via upload

---
+++ Quiz.py
@@ -0,0 +1,22 @@
+## 3.3.8 TESTE DA SEÇÃO ##
+#
+# Pergunta 1: Qual é a saída do trecho de código?
+x = 1
+y = 0
+ 
+z = ((x == y) and (x == y)) or not(x == y)
+print(not(z))
+
+# Pergunta 2: Qual é a saída do trecho de código?
+
+x = 4
+y = 1
+ 
+a = x & y
+b = x | y
+c = ~x  # tricky!
+d = x ^ 5
+e = x >> 2
+f = x << 2
+ 
+print(a, b, c, d, e, f)
